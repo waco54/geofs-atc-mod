17b1d2e38134b34fd159fbd676c5a0003a3213b6
Mk <matveyk201015@gmail.com>
2024-08-04 17:09:27 -0400

    un-minify for testing purposes

--- userscript.js
+++ userscript.js
@@ -10,41 +10,118 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    function atc() {
-    let t = {
-        rg: "roger", c: "cleared", pb: "pushback", ps: "push and start", l: "left", r: "right", ctr: "center",
-        n: "north", e: "east", s: "south", w: "west", ne: "northeast", nw: "northwest", se: "southeast", sw: "southwest",
-        af: "affirmative", ng: "negative", tx: "taxi", hld: "hold", hls: "hold short", hcp: "hold current position",
-        lup: "line up", luw: "line up and wait", rwy: "runway", m: "maintain", cm: "climb maintain", dm: "descend maintain",
-        ifr: "IFR", vfr: "VFR", trf: "traffic", tn: "turn", fnl: "final", nm: "mile",
-        nms: "miles", dpg: "departing", dpt: "departure", arg: "arriving", arr: "arrival", tkf: "for takeoff", lnd: "to land",
-        f: "face", ga: "go around", xp: "expect", cl: "cancel", hlp: "holding pattern"
+// // add atc button (closed for testing purposes)
+// let atcButton = document.createElement("button");
+// atcButton.textContent = "ATC";
+
+// // Attach the click event listener to the button
+// atcButton.addEventListener("click", atc);
+
+// // Find the target element where the button should be inserted
+// let targetElement = document.querySelector("body > div.geofs-ui-bottom");
+
+// // Insert the button before the specified child element
+// let referenceElement = document.querySelector("body > div.geofs-ui-bottom > div.geofs-ui-bottom-box.geofs-f-standard-ui");
+// if (targetElement && referenceElement) {
+//     targetElement.insertBefore(atcButton, referenceElement);
+// }
+
+function atc() {
+    const shorthandCommands = {
+        rg: 'roger',
+        c: 'cleared',
+        pb: 'pushback',
+        ps: 'push and start',
+        l: 'left',
+        r: 'right',
+        ctr: 'center',
+        n: 'north',
+        e: 'east',
+        s: 'south',
+        w: 'west',
+        ne: 'northeast',
+        nw: 'northwest',
+        se: 'southeast',
+        sw: 'southwest',
+        af: 'affirmative',
+        ng: 'negative',
+        tx: 'taxi',
+        hld: 'hold',
+        hls: 'hold short',
+        hcp: 'hold current position',
+        lup: 'line up',
+        luw: 'line up and wait',
+        rwy: 'runway',
+        m: 'maintain',
+        cm: 'climb maintain',
+        dm: 'descend maintain',
+        ifr: 'ifr',
+        vfr: 'vfr',
+        trf: 'traffic',
+        tn: 'turn',
+        fnl: 'final',
+        nm: 'mile',
+        nms: 'miles',
+        dpg: 'departing',
+        dpt: 'departure',
+        arg: 'arriving',
+        arr: 'arrival',
+        tkf: 'for take off',
+        lnd: 'to land',
+        f: "face",
+        ga: "go around",
+        xp: "expect",
+        cl: "cancel",
+        hlp: "enter holding pattern",
     };
 
-    let e = {
-        A: "Alpha", B: "Bravo", C: "Charlie", D: "Delta", E: "Echo", F: "Foxtrot", G: "Golf", H: "Hotel", I: "India",
-        J: "Juliett", K: "Kilo", L: "Lima", M: "Mike", N: "November", O: "Oscar", P: "Papa", Q: "Quebec", R: "Romeo",
-        S: "Sierra", T: "Tango", U: "Uniform", V: "Victor", W: "Whiskey", X: "X-ray", Y: "Yankee", Z: "Zulu"
+    const phoneticAlphabet = {
+        A: 'Alpha',
+        B: 'Bravo',
+        C: 'Charlie',
+        D: 'Delta',
+        E: 'Echo',
+        F: 'Foxtrot',
+        G: 'Golf',
+        H: 'Hotel',
+        I: 'India',
+        J: 'Juliett',
+        K: 'Kilo',
+        L: 'Lima',
+        M: 'Mike',
+        N: 'November',
+        O: 'Oscar',
+        P: 'Papa',
+        Q: 'Quebec',
+        R: 'Romeo',
+        S: 'Sierra',
+        T: 'Tango',
+        U: 'Uniform',
+        V: 'Victor',
+        W: 'Whiskey',
+        X: 'X-ray',
+        Y: 'Yankee',
+        Z: 'Zulu',
     };
 
-    (function r() {
-        let n = prompt("What is your message?");
-        if (!n) return;
+    function replaceShorthand() {
+        let message = prompt('What is your message?');
+        if (!message) return;
 
-        let a = n.split(" ").map(r => {
-            // Runway: Check for a 2-digit number followed by 'l' or 'r' and capitalize
-            r = r.replace(/(\d{2})([lr])/gi, (match, num, letter) => num + letter.toUpperCase());
-            let n = t[r] || r;
-            if (!/^\d{2}[LR]$/.test(r)) {
-                return n.split("").map(t => e[t] || t).join("");
-            }
-            return n;
-        }).join(" ");
+        let words = message.split(' ');
+        let replacedMessage = words.map(word => {
 
-        console.log(a);
-    })();
+            let replacedWord = shorthandCommands[word] || word;
+
+            replacedWord = replacedWord.split('').map(char => {
+                return phoneticAlphabet[char] || char;
+            }).join('');
+
+            return replacedWord;
+        }).join(' ');
+
+        console.log(replacedMessage);
+    }
+
+    replaceShorthand();
 }
-atc();
-})();
