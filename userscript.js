206691f52ac2303677554000e6cbd12af1449797
Mk <matveyk201015@gmail.com>
2024-08-04 15:57:12 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -18,7 +18,7 @@
         n: "north", e: "east", s: "south", w: "west", ne: "northeast", nw: "northwest", se: "southeast", sw: "southwest",
         af: "affirmative", ng: "negative", tx: "taxi", hld: "hold", hls: "hold short", hcp: "hold current position",
         lup: "line up", luw: "line up and wait", rwy: "runway", m: "maintain", cm: "climb maintain", dm: "descend maintain",
-        ifr: "instrument flight rules", vfr: "visual flight rules", trf: "traffic", tn: "turn", fnl: "final", nm: "mile",
+        ifr: "IFR", vfr: "VFR", trf: "traffic", tn: "turn", fnl: "final", nm: "mile",
         nms: "miles", dpg: "departing", dpt: "departure", arg: "arriving", arr: "arrival", tkf: "for takeoff", lnd: "to land",
         f: "face", ga: "go around", xp: "expect", cl: "cancel", hlp: "holding pattern"
     };
