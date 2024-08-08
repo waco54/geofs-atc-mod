8bd1728ae7e7bcc59c9d41da80407be732c61d5e
Mk <matveyk201015@gmail.com>
2024-08-08 16:07:28 -0400

    added tampermonkey comments

--- src/userscript.js
+++ src/userscript.js
@@ -1,3 +1,15 @@
+// ==UserScript==
+// @name         GeoFS ATC Mod
+// @namespace    http://tampermonkey.net/
+// @version      1.2
+// @description  GeoFS ATC mod
+// @author       mk158
+// @match        https://geo-fs.com/geofs.php*
+// @match        https://*.geo-fs.com/geofs.php*
+// @icon         https://github.githubassets.com/favicons/favicon.svg
+// @grant        none
+// ==/UserScript==
+
 let script = document.createElement('script');
 script.src = "https://mk158.github.io/geofs-atc-mod/src/main.js";
 document.head.appendChild(script);
