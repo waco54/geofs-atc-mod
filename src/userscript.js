3533e3393865929a4032cc415205bcf34d299733
Mk <matveyk201015@gmail.com>
2024-08-22 19:26:58 -0400

    Update userscript.js

--- src/userscript.js
+++ src/userscript.js
@@ -11,5 +11,5 @@
 // ==/UserScript==
 
 let script = document.createElement('script');
-script.src = "https://mk158.github.io/geofs-atc-mod/src/main.js";
+script.src = "https://waco54.github.io/geofs-atc-mod/src/main.js";
 document.head.appendChild(script);
